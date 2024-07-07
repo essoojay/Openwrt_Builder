497012e4d47c15ccf9b5199968cc26c731f05fb6
essoojay <65573284+essoojay@users.noreply.github.com>
2024-07-07 22:34:37 +0100

    Update immortalwrt-part1.sh

--- sh/immortalwrt-part1.sh
+++ sh/immortalwrt-part1.sh
@@ -17,7 +17,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # rm -rf feeds.conf.default
 # touch feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small-package' >>feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' >>feeds.conf.default
 echo 'src-git modemfeed https://github.com/koshev-msk/modemfeed.git' >>feeds.conf.default
 # echo 'src-git linkease_nas https://github.com/linkease/nas-packages-luci' >>feeds.conf.default
